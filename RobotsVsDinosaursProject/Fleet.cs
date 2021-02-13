307241843f418153e74d975ec72a89e8ba497add
Brandon Prange <bprange93@gmail.com>
2021-02-13 11:45:15 -0600

    Able to add robots to the fleet, check to status of the fleet, and get a list of the fleet.

--- RobotsVsDinosaursProject/Fleet.cs
+++ RobotsVsDinosaursProject/Fleet.cs
@@ -6,25 +6,45 @@ using System.Threading.Tasks;
 
 namespace RobotsVsDinosaursProject
 {
+    //member variables
     class Fleet
     {
         public List<Robot> robots;
-        public Weapon weapon;
-
+        public bool fleetStanding;
+        public string fleetName;
+        public double fleetHealth;
+       
+        //constructor
         public Fleet()
         {
-
-
             robots = new List<Robot>();
-
-            Robot alpha = new Robot("Alpha", 200, 250, weapon);
-            Robot beta = new Robot("Beta", 100, 200, weapon);
-            Robot omega = new Robot("Omega", 200, 200, weapon);
-
-            robots.Add(alpha);
-            robots.Add(beta);
-            robots.Add(omega);
+            fleetStanding = true;
+                        
         }
+
+        //methods
+        public void AddRobotToFleet(Robot robot)
+        {
+            robots.Add(robot);
+        }
+
+        //checks robots still standing
+        public double CheckFleet()
+        {
+            double fleetHealth;
+            fleetHealth = robots.Sum(robots => robots.robotHealth);
+            return fleetHealth;
+        }
+
+        public void ListFleet()
+        {
+            foreach(Robot robot in robots)
+            {
+                Console.WriteLine($"Name: {robot.robotName} Health {robot.robotHealth} Power Level {robot.robotPowerLevel}");
+                Console.WriteLine($"Weapon: {robot.weapon.type} Attack Power: {robot.weapon.attackPower}");
+            }
+        }
+
     }
 }
 
